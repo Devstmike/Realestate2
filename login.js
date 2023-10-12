632acc28eb70bb0dfe295b0be22228379586227e
Devstmike <139519652+Devstmike@users.noreply.github.com>
2023-10-12 15:37:47 +0100

    Login page added

---
+++ login.js
@@ -0,0 +1,6 @@
+const submitButton =  document.querySelector("img");
+submitButton.addEventListener("click",() => {
+    
+    // window.location = "https://www.upwork.com";
+    // alert("workinf");
+})
